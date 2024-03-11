9ae2d789e1860740a597b1eed32709dd9f6e7b2a
hugo-lui <hugo_lui@rogers.com>
2024-03-10 23:00:10 -0400

    initialize instances

--- api/test.py
+++ api/test.py
@@ -1,5 +1,14 @@
 import boto3
 
 ec2 = boto3.client("ec2")
-response = ec2.describe_instances()
-print(response)
+
+instancesResponse = ec2.describe_instances()
+print(instancesResponse)
+
+ec2Response = ec2.run_instances(
+    ImageId = "ami-022661f8a4a1b91cf",
+    InstanceType = "t2.micro",
+    MaxCount = 1,
+    MinCount = 1,
+)
+print(ec2Response)
